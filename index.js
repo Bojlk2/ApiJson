4a0efc17533cc3940b7647010506709bbca1a309
Job <bojlk2@hotmail.com>
2022-02-02 19:25:57 -0600

    CRUD koders mentors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const fsPromise = require('fs/promises')
+const fs = require('fs/promises')
 const fileShow = 'text.txt'
 
 // app.get('/file', (request, response) => {
@@ -26,7 +26,7 @@ const fileShow = 'text.txt'
 
 // Con Promesas (then-catch)
 app.get('/file-promise', (request, response) => {
-    fsPromise.readFile(fileShow, 'utf8')
+    fs.readFile(fileShow, 'utf8')
     .then((data) => {
         response.send(data)
     })
@@ -37,26 +37,26 @@ app.get('/file-promise', (request, response) => {
 
 //Con promesas (async/await)
 app.get('/file-async', async (request, response) => {
-    const data = await fsPromise.readFile(fileShow, 'utf8')
+    const data = await fs.readFile(fileShow, 'utf8')
     response.send(data)
 })
 
 //
-app.get('/koders', async (request, response) => {
-    const data = await fsPromise.readFile('kodemia.json', 'utf8')
-    const dataParsed = JSON.parse(data)
-    response.json(dataParsed.koders)
-})
+// app.get('/koders', async (request, response) => {
+//     const data = await fs.readFile('kodemia.json', 'utf8')
+//     const dataParsed = JSON.parse(data)
+//     response.json(dataParsed.koders)
+// })
 
 //Get /koders/emilio
 //Get /koders/sara
 app.get('/koders/:id', async (request, response) => {
-    const id = parseInt(request.params.id)
-    const data = await fsPromise.readFile('kodemia.json', 'utf8')
+    const id = parseInt(request.params.id) 
+    const data = await fs.readFile('kodemia.json', 'utf8')
     const db = JSON.parse(data)
 
     const koderFound = db.koders.filter((koder) => {
-        return koder.id === id //Normalización
+        return koder.id === id
         
     })
 
@@ -66,7 +66,7 @@ app.get('/koders/:id', async (request, response) => {
 
 app.get('/koders/sex/:sex', async (request, response) => {
     const sex = request.params.sex
-    const data = await fsPromise.readFile('kodemia.json', 'utf8')
+    const data = await fs.readFile('kodemia.json', 'utf8')
     const db = JSON.parse(data)
 
     const koderSex = db.koders.filter((koder) => {
@@ -75,9 +75,180 @@ app.get('/koders/sex/:sex', async (request, response) => {
     response.json(koderSex)
 })
 
+
+
+// Hacer un endpoint que al llamarlo nos regrese el contenido de un archivo text.txt
+// GET /file
+
+
+app.get('/koders', async (request, response) => {
+    const data = await fs.readFile('kodemia.json', 'utf8')
+    const db = JSON.parse(data)
+    let kodersFound = db.koders
+
+    if(request.query.max_age) {
+        kodersFound = kodersFound.filter((koder) => {
+            return koder.age <= parseInt(request.query.max_age)
+        })
+    }
+
+    response.json(kodersFound)
+})
+
+//Important para leer el body del request
+app.use(express.json())
+
+
+app.post('/koders', async (request, response) => {
+    const data = await fs.readFile('kodemia.json', 'utf8')
+    const db = JSON.parse(data)
+
+    const newKoderId = db.koders.length + 1
+    const newKoderData ={
+        id: newKoderId,
+        ...request.body
+    }
+
+    db.koders.push(newKoderData)
+
+    const dbAsString = JSON.stringify(db, '\n', 2)
+    await fs.writeFile('kodemia.json', dbAsString, 'utf8')
+
+    response.json(db.koders)
+})
+
+app.delete('/koders/:id', async (request ,response) => {
+    const id =  parseInt(request.params.id)
+    const data = await fs.readFile('kodemia.json', 'utf8')
+    const db = JSON.parse(data)
+    
+    const newKodersArray = db.koders.filter((koder) => id != koder.id)
+    db.koders = newKodersArray
+
+    const dbAsString = JSON.stringify(db, '\n', 2)
+    await fs.writeFile('kodemia.json', dbAsString, 'utf8')
+
+    response.json(db.koders)
+})
+
+app.patch('/koders/:id', async (request, response) => {
+    const id = parseInt(request.params.id)
+
+    if(isNaN(id)) {
+        response
+            .status(400)
+            .json({
+                message: "Id must be a number"
+            })
+            return
+    }
+
+    const data = await fs.readFile('kodemia.json', 'utf8')
+    const db = JSON.parse(data)
+
+    const koderFoundIndex = db.koders.findIndex((koder) => id === koder.id)
+
+    if(koderFoundIndex < 0) {
+        response.status(404)
+        response.json({
+            message: 'Koder not found'
+        })
+        return
+    }
+
+    db.koders[koderFoundIndex] = {
+        ...db.koders[koderFoundIndex],
+        ...request.body
+    }
+
+    const dbAsString = JSON.stringify(db, '\n', 2)
+    await fs.writeFile('kodemia.json', dbAsString, 'utf8')
+
+    response.json(db.koders[koderFoundIndex])
+
+
+})
+
 app.listen(8080, () => {
     console.log('Server is listening')
 })
 
-// Hacer un endpoint que al llamarlo nos regrese el contenido de un archivo text.txt
-// GET /file
+
+//Mentors
+app.post('/mentors', async (request, response) => {
+    const data = await fs.readFile('kodemia.json', 'utf8')
+    const db = JSON.parse(data)
+
+    const newMentorId = db.mentors.length + 1
+    const newMentorData ={
+        id: newMentorId,
+        ...request.body
+    }
+
+    db.mentors.push(newMentorData)
+
+    const dbAsString = JSON.stringify(db, '\n', 2)
+    await fs.writeFile('kodemia.json', dbAsString, 'utf8')
+
+    response.json(db.mentors)
+})
+
+app.delete('/mentors/:id', async (request ,response) => {
+    const id =  parseInt(request.params.id)
+    const data = await fs.readFile('kodemia.json', 'utf8')
+    const db = JSON.parse(data)
+    
+    const newMentorsArray = db.mentors.filter((mentor) => id != mentor.id)
+    db.mentors = newMentorsArray
+
+    const dbAsString = JSON.stringify(db, '\n', 2)
+    await fs.writeFile('kodemia.json', dbAsString, 'utf8')
+
+    response.json(db.mentors)
+})
+
+app.patch('/mentors/:id', async (request, response) => {
+    const id = parseInt(request.params.id)
+
+    if(isNaN(id)) {
+        response
+            .status(400)
+            .json({
+                message: "Id must be a number"
+            })
+            return
+    }
+
+    const data = await fs.readFile('kodemia.json', 'utf8')
+    const db = JSON.parse(data)
+
+    const mentorFoundIndex = db.mentors.findIndex((mentor) => id === mentor.id)
+
+    if(mentorFoundIndex < 0) {
+        response.status(404)
+        response.json({
+            message: 'Mentor not found'
+        })
+        return
+    }
+
+    db.mentors[mentorFoundIndex] = {
+        ...db.mentors[mentorFoundIndex],
+        ...request.body
+    }
+
+    const dbAsString = JSON.stringify(db, '\n', 2)
+    await fs.writeFile('kodemia.json', dbAsString, 'utf8')
+
+    response.json(db.mentors[mentorFoundIndex])
+
+
+})
+
+app.get('/mentors', async (request, response) => {
+    const data = await fs.readFile('kodemia.json', 'utf8')
+    const db = JSON.parse(data)
+    let mentorsFound = db.mentors
+
+    response.json(mentorsFound)
+})
